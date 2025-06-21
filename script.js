c791a17d84d2825f493204d68c048f11d168d482
NitinRana08 <nitinr8229@gmail.com>
2025-06-21 18:19:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const color = ["red", "green", "blue", "yellow", "purple"];
+// var first = color[0]
+// var second = color[1];
+// var third = color[2];
+// var forth = color[3]
+
+const [first,second,third] = color;
+const nitin = [x1,x2]=color
+console.log(nitin)
